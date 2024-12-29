262a99a5f4f65c43f76cf96762e3572d920d78ae
nelith-2002 <nelith.20230664@iit.ac.lk>
2024-12-29 21:20:58 +0530

    first commit

---
+++ real-estate-wesite-001/src/components/displayicons.jsx
@@ -0,0 +1,14 @@
+import './displayicons.css';
+import { FaYoutube } from "react-icons/fa";
+
+function DisplayIcons() {
+  return (
+    <div>
+      <h1>
+        youtube <FaYoutube className="yticon" />
+      </h1>
+    </div>
+  );
+}
+
+export default DisplayIcons;
---
+++ real-estate-wesite-001/src/home.jsx
@@ -0,0 +1,11 @@
+import DisplayIcons from './components/displayicons';
+
+function Home() {
+  return (
+    <div>
+      <DisplayIcons />
+    </div>
+  );
+}
+
+export default Home;
