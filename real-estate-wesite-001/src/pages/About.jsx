4cb66fe3239e9f8f77b98dcf772e81bdbb83ea81
nelith-2002 <nelith.20230664@iit.ac.lk>
2025-01-02 21:33:03 +0530

    create About page

---
+++ real-estate-wesite-001/src/pages/About.jsx
@@ -0,0 +1,12 @@
+
+
+function About() {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>Learn more about our services and team.</p>
+    </div>
+  );
+}
+
+export default About; // Ensure default export is present
