8b3b01ac218c57403cb1d522f1de1d4caa2e68a2
nelith-2002 <nelith.20230664@iit.ac.lk>
2025-01-02 21:23:04 +0530

    impment pagenavigator which is navigate throgh the pages

--- real-estate-wesite-001/src/pages/Pagenavigator.jsx
+++ real-estate-wesite-001/src/pages/Pagenavigator.jsx
@@ -1,12 +1,29 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Header from '../components/header/Header';
 import Footer from '../components/footer/Footer';
+import Home from '../Home';
+import About from './About';
+import Contact from './Contact';
+import SearchResults from './SearchResult';
 
-export default function Pagenavigator() {
+function Pagenavigator() {
   return (
-    <>
-      <Header />
-      {/* Add any page-specific content here */}
-      <Footer />
-    </>
+    <Router>
+      <div className="app-container">
+        <Header />
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/search" element={<SearchResults />} />
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   );
 }
+
+export default Pagenavigator;
