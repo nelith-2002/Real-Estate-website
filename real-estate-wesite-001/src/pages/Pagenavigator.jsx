6cc32602cc31990817b768e6441d69dea6781d6c
nelith-2002 <nelith.20230664@iit.ac.lk>
2025-01-03 13:37:41 +0530

    add page navigators and create a synchroniztion(create a synchroniztion in between Favourite List side bar and Favourite List Page which is in the MyList

--- real-estate-wesite-001/src/pages/Pagenavigator.jsx
+++ real-estate-wesite-001/src/pages/Pagenavigator.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Header from '../components/header/Header';
 import Footer from '../components/footer/Footer';
@@ -5,8 +6,33 @@ import Home from '../Home';
 import About from './About';
 import Contact from './Contact';
 import SearchResults from './SearchResult';
+import FavouriteList from './FavouriteList';  
+import Prop1Page from './PropertyPages/Prop1Page';
+import Prop2Page from './PropertyPages/Prop2Page';
+import Prop3Page from './PropertyPages/Prop3Page';
+import Prop4Page from './PropertyPages/Prop4Page';
+import Prop5Page from './PropertyPages/Prop5Page';
+import Prop6Page from './PropertyPages/Prop6Page';
+import Prop7Page from './PropertyPages/Prop7Page';
+
 
 function Pagenavigator() {
+  const [favorites, setFavorites] = useState([]);
+
+  const addToFavorites = (property) => {
+    if (!favorites.some((fav) => fav.id === property.id)) {
+      setFavorites([...favorites, property]);
+    }
+  };
+
+  const removeFromFavorites = (propertyId) => {
+    setFavorites(favorites.filter((fav) => fav.id !== propertyId));
+  };
+
+  const clearFavorites = () => {
+    setFavorites([]);
+  };
+
   return (
     <Router>
       <div className="app-container">
@@ -16,7 +42,34 @@ function Pagenavigator() {
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
-            <Route path="/search" element={<SearchResults />} />
+            <Route
+              path="/search"
+              element={
+                <SearchResults
+                  favorites={favorites}
+                  addToFavorites={addToFavorites}
+                  removeFromFavorites={removeFromFavorites}
+                  clearFavorites={clearFavorites}
+                />
+              }
+            />
+            <Route
+              path="/favourites"
+              element={
+                <FavouriteList
+                  favorites={favorites}
+                  removeFromFavorites={removeFromFavorites}
+                  clearFavorites={clearFavorites}
+                />
+              }
+            />
+            <Route path="/property-prop1" element={<Prop1Page />} />
+            <Route path="/property-prop2" element={<Prop2Page />} />
+            <Route path="/property-prop3" element={<Prop3Page />} />
+            <Route path="/property-prop4" element={<Prop4Page />} />
+            <Route path="/property-prop5" element={<Prop5Page />} />
+            <Route path="/property-prop6" element={<Prop6Page />} />
+            <Route path="/property-prop7" element={<Prop7Page />} />
             <Route path="*" element={<Navigate to="/" />} />
           </Routes>
         </main>
