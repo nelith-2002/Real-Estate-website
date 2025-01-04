539add327cd4e54d3b80bff3b809d9328f5e0adc
nelith-2002 <nelith.20230664@iit.ac.lk>
2025-01-04 10:24:49 +0530

    change the home.jsx accodring to some changes

--- real-estate-wesite-001/src/home.jsx
+++ real-estate-wesite-001/src/home.jsx
@@ -1,10 +1,16 @@
-import SearchBar from './components/Search/SearchBar'; // Correct import path
+import SearchBar from './components/Search/SearchBar'; 
+import FeaturedPropertyTypes from './components/FeaturedPropertyTypes/FeaturedPropertyTypes';
+import RecentPropertyListed from './components/RecentPropertyListed/RecentPropertyListed'; 
+
 
 
 function Home() {
   return (
     <div>
       <SearchBar />
+      <FeaturedPropertyTypes />
+       <RecentPropertyListed />
+     
       
      
       
