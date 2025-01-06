361c9938f3fe0f752ecccae776e20c608992f95a
nelith-2002 <nelith.20230664@iit.ac.lk>
2025-01-06 09:04:04 +0530

    add csp and html encoding for the web site for ensure that the content of the page is properly encoded and that the content of the page is properly decoded and that the content of the page is properly encoded and decoded properly when the content of the page is decoded and decoded properly when the content of the page is decoded and decoded properly when the content of the page is decoded and decoded properly when the content of the page is decoded and decoded properly when the content of the page is decoded and decoded properly when the content of the page is decoded and decoded properly when the content of the page is decoded and decoded

--- real-estate-wesite-001/src/App.jsx
+++ real-estate-wesite-001/src/App.jsx
@@ -1,8 +1,23 @@
+// Updated App.js with Google Maps CSP Fix
 import './App.css';
 import Pagenavigator from './pages/Pagenavigator';
+import { Helmet } from 'react-helmet';
 
 function App() {
-  return <Pagenavigator />;
+  return (
+    <>
+      {/* Add Content Security Policy with Google Maps Fix */}
+      <Helmet>
+        <meta
+          httpEquiv="Content-Security-Policy"
+          content="default-src 'self'; script-src 'self' https://maps.googleapis.com; img-src 'self' data: https://maps.gstatic.com; style-src 'self' 'unsafe-inline'; font-src 'self'; frame-src https://www.google.com https://maps.googleapis.com;"
+        />
+      </Helmet>
+
+      {/* Main Page Navigator */}
+      <Pagenavigator />
+    </>
+  );
 }
 
 export default App;
