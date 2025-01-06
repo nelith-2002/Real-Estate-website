60759d6197a7ad76d58c402590b9aae6ac952864
nelith-2002 <nelith.20230664@iit.ac.lk>
2025-01-06 10:29:49 +0530

    take a commets for home and index files

--- real-estate-wesite-001/src/home.jsx
+++ real-estate-wesite-001/src/home.jsx
@@ -1,15 +1,19 @@
-import SearchBar from './components/Search/SearchBar'; 
+// Importing key components for the Home page
+import SearchBar from './components/Search/SearchBar'; // Search bar component
 import FeaturedPropertyTypes from './components/FeaturedPropertyTypes/FeaturedPropertyTypes';
 import RecentPropertyListed from './components/RecentPropertyListed/RecentPropertyListed'; 
 
-
+/* 
+   Home Component
+   Serves as the landing page of the application. Displays the search bar, featured properties, and recent property listings.
+*/
 
 function Home() {
   return (
     <div>
-      <SearchBar />
-      <FeaturedPropertyTypes />
-       <RecentPropertyListed />
+      <SearchBar /> {/* Search bar for finding properties */}
+      <FeaturedPropertyTypes /> {/* Section showcasing property categories */}
+       <RecentPropertyListed />  {/* Section displaying recently listed properties */}
      
       
      
