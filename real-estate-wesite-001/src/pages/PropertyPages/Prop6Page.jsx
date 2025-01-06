bc02be94beada2b9202b4b24d8c0f3c0128c16ef
nelith-2002 <nelith.20230664@iit.ac.lk>
2025-01-06 10:22:46 +0530

    take a comments for the property page

--- real-estate-wesite-001/src/pages/PropertyPages/Prop6Page.jsx
+++ real-estate-wesite-001/src/pages/PropertyPages/Prop6Page.jsx
@@ -2,10 +2,15 @@ import { Carousel, Tab, Tabs, Container, Row, Col } from 'react-bootstrap';
 import properties from '../../components/data/properties.json';
 import './PropPage.css';
 
+/* 
+   Prop1Page Component
+   Displays detailed information about a specific property, including a carousel for images, descriptive tabs, and a Google Map location.
+*/
 const Prop6Page = () => {
   // Fetch the property details from properties.json based on the id
   const property = properties.properties.find((prop) => prop.id === 'prop6');
 
+  // Array of images for the carousel
   const carouselImages = [
     '/src/assets/images/prop6pic1small.png',
     '/src/assets/images/prop6Carousel1.jpg',
@@ -19,7 +24,6 @@ const Prop6Page = () => {
 
   return (
     <Container className="py-5">
-      {/* Property Title */}
       <Row className="justify-content-center">
         <Col xs={12} className="text-center">
           <h1 className="mb-4">{property.type} in {property.location}</h1>
